6cea2e70f5430dbfb12544351aa6561a8c5f2184
danclark1820 <danclark1820@gmail.com>
2015-04-20 14:36:54 -0400

    fortune shows up when cookie is clicked

---
+++ app/assets/javascripts/fortunes.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//   $('.fortune').hide()
+// });
+
+$(document).ready(function(){
+  $('.fortune').hide();
+
+  $('.fortune_cookie').click(function(){
+    $('.fortune').show();
+  });
+
+  $('.fortune_cookie').click(function(){
+    $('.fortune_cookie').hide();
+  });
+});
