3ca4733654090ba16da63ff131102b3bd3821847
danclark1820 <danclark1820@gmail.com>
2015-04-23 11:36:11 -0400

    move note forum to fortune show page

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,5 +1,10 @@
 class FortunesController < ApplicationController
   def show
     @fortune = Fortune.offset(rand(Fortune.count)).first
+    @note = @fortune.notes.build
+  end
+
+  def fortune_params
+    params.require(:fortune).permit(:note)
   end
 end
