e388ca352e6cd88fd9a56b243aabfcade1771d76
danclark1820 <danclark1820@gmail.com>
2015-04-23 15:39:31 -0400

    option to create account if not logged in

--- app/assets/javascripts/fortunes.js
+++ app/assets/javascripts/fortunes.js
@@ -4,9 +4,14 @@
 
 $(document).ready(function(){
   $('.fortune').hide();
+  $('.fortune_note').hide();
 
   $('.fortune_cookie').click(function(){
     $('.fortune').show();
+  });
+
+  $('.fortune_cookie').click(function(){
+    $('.fortune_note').show();
   });
 
   $('.fortune_cookie').click(function(){
