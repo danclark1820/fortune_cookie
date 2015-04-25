4bb6d79f56b2220046db19c695ca86859b66281f
danclark1820 <danclark1820@gmail.com>
2015-04-25 09:59:15 -0400

    handle unlogged in user in controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,17 +5,22 @@ class NotesController < ApplicationController
     @note = @fortune.notes.build
   end
 
+  def note_redirect
+    flash[:notice] = "Good thought, hope you were able to clear your mind a bit"
+    if @note.user_id != nil
+      redirect_to user_path(params[:note][:user_id])
+    else
+      flash[:notice] = "You never know when you might want to revisit an idea, why not sign up?"
+      redirect_to new_user_registration_path
+    end
+  end
+
   def create
     @fortune = Fortune.find(params[:fortune_id])
     @note = @fortune.notes.build(note_params)
     if @note.save
-      flash[:notice] = "Good thought, hope you were able to clear your mind a bit"
-      #this next step should encourage the user to leave for the day, and come back tomorrow
-      redirect_to user_path(params[:note][:user_id])
+      note_redirect
     else
-      #this is where we can check if the person is a user yet
-      #but there should be a way to save the notes of users that
-      #do not want to sign in
       flash[:notice] = "Say anything, there must be something on your mind"
       redirect_to new_fortune_note_path(params[:fortune_id])
     end
