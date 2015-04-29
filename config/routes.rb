db57a4fb608c2d4be46904ac5f204aa9c1995210
danclark1820 <danclark1820@gmail.com>
2015-04-29 16:54:46 -0400

    fix static pages naming, align text on user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  root 'fortunes#show'
+  root 'staticpages#home'
   devise_for :users
 
   resources :users, only: [:show]
 
-  resources :landings, only: [:index, :show]
-
   resources :fortunes, only: [:show, :index] do
     resources :notes
   end
+
+  get '/home' => 'static_pages#home'
+  get '/science' => 'static_pages#science'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
